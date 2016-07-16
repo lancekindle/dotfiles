052e996527e6efd7be81cb1c2243a052806dbba3
Lance Kindle <lance.kindle@gmail.com>
2016-07-16 13:34:56 -0700

    update installation scripts

--- keyboard/bin/dvorak-qwerty/install.sh
+++ keyboard/bin/dvorak-qwerty/install.sh
@@ -21,12 +21,21 @@ DIRECTORY_OWNER=$(namei -o $PWD | tail -n 1 | awk '{if ($2) print $2}')
 chown $DIRECTORY_OWNER -R $NAME
 
 echo "Copying Dvorak-Qwerty to types"
+# install many dvorak-qwerty variants in the "us" symbols
+# append to end of file
+# this is OLD, the newer symbols file is separate
+#if [[ ! `cat $CONF/symbols/us | grep "dvorak-qwerty"` ]]; then
+#    echo "adding dvorak variants to US layout"
+#    cat $SCRIPT_DIR/symbols/us >> $CONF/symbols/us
+#fi
+
 # if you ever change these files, you'll need to remove xkb's compiled cache of
 # keymappings: sudo rm -f /var/lib/xkb/*.xkm
 # link symbol file to xkb symbols
 symbol_file="$SCRIPT_DIR/symbols/usdvq"
 system_file="/usr/share/X11/xkb/symbols/usdvq"
 ln -s $symbol_file $system_file
+
 # link dvoraq-qwerty type to xkb types
 # this is the part that makes redirection possible
 symbol_file="$SCRIPT_DIR/types/dvorak-qwerty"
@@ -52,24 +61,7 @@ if [[ ! `cat $CONF/types/complete | grep "dvorak-qwerty"` ]]; then
     sed -i '/};/i \ \ \ \ include "dvorak-qwerty"' $CONF/types/complete
 fi
 
-# install many dvorak-qwerty variants in the "us" symbols
-# append to end of file
-if [[ ! `cat $CONF/symbols/us | grep "dvorak-qwerty"` ]]; then
-    echo "adding dvorak variants to US layout"
-    cat $SCRIPT_DIR/symbols/us >> $CONF/symbols/us
-fi
-
-# copy commented out media-codes to end of inet layout.
-# these media codes will have to be individually added as the user figures out
-# which media keys do not work in dvorak-qwerty mode
-if [[ ! `cat $CONF/symbols/inet | grep "dvorak-qwerty"` ]]; then
-    echo "adding commented-out media-key codes to symbols/inet \
-         for media keys that do not work, you must edit this \
-         inet file (see bottom of it for details)"
-    cat $SCRIPT_DIR/symbols/inet >> $CONF/symbols/inet
-fi
-
 # install new layout variants in rules section, so that it can be loaded
-# with setxkbmap
-# this is the tough part. What to do....
+# with setxkbmap. This is handled by ./rules/install_rules.py
+./rules/install_rules.py
 exit 0
--- keyboard/bin/dvorak-qwerty/rules/install.py
+++ keyboard/bin/dvorak-qwerty/rules/install.py
@@ -3,28 +3,27 @@
 # this script installs the usdvq layouts to the base.xml and evdev.xml
 # files in /usr/share/X11/xkb/rules
 
-# this worked but left out the top two comments on document which specified
-# it's type.
-
 rules_dir = '/usr/share/X11/xkb/rules/'
 system_base_xml = rules_dir + 'base.xml'
 system_evdev_xml = rules_dir + 'evdev.xml'
 #
 # using xml.etree is simpler BUT it does not preserve comments :(
-# 
+# (which includes doctype)
+#
 # from xml.etree import ElementTree as ET
-# 
+#
 # partial_xml = ET.parse('base.xml')
 # dvorak_qwerty_layout = partial_xml.getroot()
-# 
+#
 # base_tree = ET.parse(base_xml)
 # root = base_tree.getroot()
-# 
+#
 # layout = root.find('.//layoutList')
 # layout.insert(1, dvorak_qwerty_layout)
 # base_tree.write('base.xml2')
 
 from xml.dom import minidom
+import warnings
 
 insert_file = 'base.xml'
 edit_file = system_base_xml
@@ -55,4 +54,39 @@ if 'Dvorak-Qwerty' not in layout_list.toxml():
         base_tree.writexml(xml_file)
 
 
-# now add list of variants to base.lst and evdev.lst
+# Add list of variants to base.lst and evdev.lst
+dvq_layout_line = '  usdvq           English (Dvorac-Qwerty)\n'
+dvq_variant_lines = """
+  dvq-simplified  usdvq: English (Dvorak-Qwerty, simplified)
+  dvorak-qwerty   usdvq: English (Dvorak Qwerty)
+  dvq-intl        usdvq: English (Dvorak-Qwerty, international with dead keys)
+  dvq-alt-intl    usdvq: English (Dvorak-Qwerty alternative international no dead keys)
+  dvq-classic     usdvq: English (Dvorak-Qwerty, classic)
+  dvqp            usdvq: English (Dvorak-Qwerty, programmer)
+"""
+for filename in [rules_dir + 'base.lst', rules_dir + 'evdev.lst']:
+    lines = None
+    with open(filename, 'r') as f:
+        lines = f.read()
+
+    # verify Dvorak-Qwerty not already installed
+    if 'Dvorak-Qwerty' in lines:
+        warnings.warn(
+            'file ' + filename + ' has "Dvorak-Qwerty" already installed'
+        )
+        continue
+
+    # Add usdvq layout to list of layouts
+    start_layout = lines.find('! layout')
+    start_us = lines.find(' us ', start_layout)
+    cut = lines.find('\n', start_us)
+    lines = lines[:cut + 1] + dvq_layout_line + lines[cut + 1:]
+
+    start_variants = lines.find('! variant')
+    last_us_variants = lines.rfind('us: English', start_variants)
+    cut = lines.find('\n', last_us_variants)
+    lines = lines[:cut + 1] + dvq_variant_lines + lines[cut + 1:]
+
+    with open(filename, 'w') as f:
+        f.write(lines)
+
